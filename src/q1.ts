0787d4e8e1ceac9debe6bc6b0072f7ed7be8648c
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2022-12-06 16:03:18 -0500

    q1 part 2 solution

--- src/q1.ts
+++ src/q1.ts
@@ -2,16 +2,28 @@ type CaloriesByElfId = Record<string, number[]>;
 type TotalCaloriesByElfId = Record<string, number>;
 type SortedTotalCaloriesByElfId = [string, number][];
 
-/** @returns ID of elf carrying the most total calories. An elf's ID corresponds to their one-based index. */
+/** @returns ID of elf carrying the most total calories. An elf's ID corresponds to their one-based index
+ *  @param inputContents - String representing the input file's contents */
 export function elfWithMostCalories(inputContents: string): string {
   const sortedTotalCalories: SortedTotalCaloriesByElfId = processInput(inputContents);
   return sortedTotalCalories[0][0];
 }
 
-/** @returns Total calories of the elf carrying the most total calories */
+/** @returns Total calories of the elf carrying the most total calories
+ *  @param inputContents - String representing the input file's contents */
 export function caloriesOfElfWithMostCalories(inputContents: string): number {
   const sortedTotalCalories: SortedTotalCaloriesByElfId = processInput(inputContents);
   return sortedTotalCalories[0][1];
+}
+
+/** @returns Total calories of the n elves carrying the most total calories
+ *  @param inputContents - String representing the input file's contents
+ *  @param numberOfElves - The number of elves carrying the most calories to include in the sum
+ *  @example `caloriesOfElvesWithMostCalories(3)` -> total calories of the top 3 elves carrying the most calories */
+export function caloriesOfElvesWithMostCalories(inputContents: string, numberOfElves: number): number {
+  const sortedTotalCalories: SortedTotalCaloriesByElfId = processInput(inputContents);
+  const slicedArray: SortedTotalCaloriesByElfId = sortedTotalCalories.slice(0, numberOfElves);
+  return slicedArray.reduce((caloriesSum, [, elfCalories]) => caloriesSum + elfCalories, 0);
 }
 
 /** @returns Total calories carried by each elf, ordered by descending total calories */
