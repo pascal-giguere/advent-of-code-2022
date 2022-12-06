1a55d86a7613632b55dba355bfc111309af3ad01
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2022-12-06 17:57:42 -0500

    part 2 wip

--- test/q2/q2.test.ts
+++ test/q2/q2.test.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { calculateEncryptedStrategyGuidePoints } from '../../src/q2';
+import { calculateEncryptedStrategyGuidePointsV1 } from '../../src/q2';
 
 const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple.txt'), 'utf-8');
 const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'), 'utf-8');
@@ -8,12 +8,12 @@ const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'),
 describe('q2', () => {
   describe('Part 1', () => {
     it('calculates the total number of points when playing an encrypted strategy guide - simple input', () => {
-      const points: number = calculateEncryptedStrategyGuidePoints(SIMPLE_INPUT);
+      const points: number = calculateEncryptedStrategyGuidePointsV1(SIMPLE_INPUT);
       expect(points).toEqual(15);
     });
 
     it('calculates the total number of points when playing an encrypted strategy guide - complex input', () => {
-      const points: number = calculateEncryptedStrategyGuidePoints(COMPLEX_INPUT);
+      const points: number = calculateEncryptedStrategyGuidePointsV1(COMPLEX_INPUT);
       expect(points).toEqual(13009);
     });
   });
