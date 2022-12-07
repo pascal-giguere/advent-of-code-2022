2e710b70cfc2c751065844150b9ef20ad4d42fb3
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2022-12-06 21:46:15 -0500

    Question 4 solution (#3)
    * q4 wip
    
    * part 1
    
    * part 2 wip
    
    * part 2

---
+++ src/q4/index.ts
@@ -0,0 +1,60 @@
+type Assignment = { start: number; end: number };
+type AssignmentsPair = [Assignment, Assignment];
+type AssignmentsPairList = AssignmentsPair[];
+
+/** @returns The number of assignment pairs that fully overlap with each other
+ *  @param inputContents - String representing the input file's contents */
+export function countFullyOverlappingPairs(inputContents: string): number {
+  const pairs: AssignmentsPairList = parseInput(inputContents);
+  return pairs.filter(isFullyOverlappingPair).length;
+}
+
+/** @returns The number of assignment pairs that partially or fully overlap with each other
+ *  @param inputContents - String representing the input file's contents */
+export function countOverlappingPairs(inputContents: string): number {
+  const pairs: AssignmentsPairList = parseInput(inputContents);
+  return pairs.filter(isOverlappingPair).length;
+}
+
+/** @returns Parsed input file with no data transform applied */
+function parseInput(inputContents: string): AssignmentsPairList {
+  const inputLines: string[] = inputContents.split('\n').filter((l) => !!l);
+  return inputLines.map(parseInputLine);
+}
+
+/** @returns Parsed line from input file with no data transform applied */
+function parseInputLine(line: string): AssignmentsPair {
+  const splitLine = line.split(',');
+  if (splitLine.length !== 2) {
+    throw Error(`Invalid input line: '${line}'`);
+  }
+  return [parseAssignment(splitLine[0]), parseAssignment(splitLine[1])];
+}
+
+/** @returns Parsed line from input file with no data transform applied */
+function parseAssignment(str: string): Assignment {
+  const splitStr = str.split('-');
+  if (splitStr.length !== 2) {
+    throw Error(`Invalid assignment string: '${str}'`);
+  }
+  return {
+    start: parseInt(splitStr[0]),
+    end: parseInt(splitStr[1]),
+  };
+}
+
+/** @returns Whether the two provided assignments fully overlap with each other */
+function isFullyOverlappingPair([assignmentA, assignmentB]: AssignmentsPair): boolean {
+  return (
+    (assignmentA.start <= assignmentB.start && assignmentA.end >= assignmentB.end) ||
+    (assignmentB.start <= assignmentA.start && assignmentB.end >= assignmentA.end)
+  );
+}
+
+/** @returns Whether the two provided assignments partially or fully overlap with each other */
+function isOverlappingPair([assignmentA, assignmentB]: AssignmentsPair): boolean {
+  return (
+    (assignmentA.end >= assignmentB.start && assignmentA.start <= assignmentB.end) ||
+    (assignmentB.end >= assignmentA.start && assignmentB.start <= assignmentA.end)
+  );
+}
---
+++ test/q4/q4.test.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { countFullyOverlappingPairs, countOverlappingPairs } from '../../src/q4';
+
+const SIMPLE_INPUT = fs.readFileSync(path.join(__dirname, 'input-simple.txt'), 'utf-8');
+const COMPLEX_INPUT = fs.readFileSync(path.join(__dirname, 'input-complex.txt'), 'utf-8');
+
+describe('q4', () => {
+  describe('Part 1', () => {
+    it('finds the number of assignment pairs that fully overlap with each other - simple input', () => {
+      const overlappingPairCount: number = countFullyOverlappingPairs(SIMPLE_INPUT);
+      expect(overlappingPairCount).toEqual(2);
+    });
+
+    it('finds the number of assignment pairs that fully overlap with each other - complex input', () => {
+      const overlappingPairCount: number = countFullyOverlappingPairs(COMPLEX_INPUT);
+      expect(overlappingPairCount).toEqual(496);
+    });
+  });
+
+  describe('Part 2', () => {
+    it('finds the number of assignment pairs that partially or fully overlap with each other - simple input', () => {
+      const overlappingPairCount: number = countOverlappingPairs(SIMPLE_INPUT);
+      expect(overlappingPairCount).toEqual(4);
+    });
+
+    it('finds the number of assignment pairs that partially or fully overlap with each other - complex input', () => {
+      const overlappingPairCount: number = countOverlappingPairs(COMPLEX_INPUT);
+      expect(overlappingPairCount).toEqual(847);
+    });
+  });
+});
